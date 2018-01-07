5472f5f75f3435bd8144db9a1b1c21458181cb7a
Marinov <icomarinov@abv.bg>
2018-01-07 21:57:52 +0200

    dd

---
+++ _171105_05_Java.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class _171105_05_Java {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        steam(n, "~");
+    }
+
+
+    static void steam(int in, String symbol) {
+
+        for(int i=1; i<=in; i++) {
+            for (int j = 1 ; j <=3 ; j++) {
+                System.out.print(symbol);
+            }
+
+            System.out.println("");
+        }
+
+    };;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
+
+}
